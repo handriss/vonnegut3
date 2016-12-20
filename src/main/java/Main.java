ada40f299bb348af64656bff3bce90b1183b1f8d
Mónika Lombos <lombos.monika@gmail.com>
2016-12-20 21:57:31 +0100

    Session object is saved into the vonnegut database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,40 @@
-import au.com.bytecode.opencsv.CSVReader;
+import entity.Book;
+import entity.enums.BookCondition;
+import entity.enums.Cover;
+import entity.enums.Status;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-/**
- * Created by handris on 12/9/16.
- */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Running");
-        // try mySQL connection on simple way
-//        ConnectionHandler connectionHandler = new ConnectionHandler();
-
 
         SessionFactory factory = new Configuration().configure("config/hibernate.cfg.xml").addAnnotatedClass(Book.class).buildSessionFactory();
         Session session = factory.getCurrentSession();
 
         try{
             // use the session object
-            CSVReader reader = new CSVReader(new FileReader("src/test/java/book_attribute.csv"));
-            String [] nextLine;
-            ArrayList<Book> books = new ArrayList<>();
-            while ((nextLine = reader.readNext()) != null) {
-//                Book tempBook = new Book(nextLine[0], nextLine[1], nextLine[2], nextLine[3], nextLine[4], nextLine[5], nextLine[6], nextLine[7], nextLine[8], nextLine[9], nextLine[10], nextLine[11], nextLine[12], nextLine[13], nextLine[14], nextLine[15], nextLine[16], nextLine[17], nextLine[18], nextLine[19]);
-                Book tempBook = new Book(nextLine[0]);
-                books.add(tempBook);
-            }
+            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
+            Date dateOfUpload = sdf.parse("12/13/2016 08:39:29");
+            Date dateOfSold = null;
+
+            Book tempBook = new Book(Status.ÁRULHATÓ, "644909", 2103982191, "Herbie Brennan", "Az ókori Egyiptom titkos története - Hogyan tűnt el egy fejlett civilizáció?", 2000, "Alexandra Kiadó", "'0609001023751", "c19", 1490, dateOfUpload, dateOfSold, "60", 1.5f, 239, Cover.FÉLMŰBŐR, BookCondition.HIÁBTLAN, "tarara", "menyhard", "romantic", "9789789789788");
 
             // start transaction
             session.beginTransaction();
 
             // save the book
-            session.save(books.get(1));
+            session.save(tempBook);
 
             // commit the transaction
             session.getTransaction().commit();
 
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (ParseException e) {
             e.printStackTrace();
         } finally{
             factory.close();
