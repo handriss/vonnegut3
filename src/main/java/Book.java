aa5e571c548549951b1be49740a3670b8f75cf83
Mónika Lombos <lombos.monika@gmail.com>
2016-12-20 17:53:57 +0100

    Added getters & setters methods and two constructors & toString method

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -69,4 +69,218 @@ public class Book {
 
     @Column(name="isbn")
     private String isbn;
+
+    // empty contructor
+    public Book() {
+    }
+    // constructor
+    public Book(String status, String bookId, String author, String title, Integer yearOfPublication, String publisher, Long barcode, String storagePlace, Integer price, Timestamp dateOfShopSale, Integer shopId, Float weight, Integer numberOfPages, String cover, String condition, String moreInfo, String uploader, String category, String isbn) {
+        this.status = status;
+        this.bookId = bookId;
+        this.author = author;
+        this.title = title;
+        this.yearOfPublication = yearOfPublication;
+        this.publisher = publisher;
+        this.barcode = barcode;
+        this.storagePlace = storagePlace;
+        this.price = price;
+        this.dateOfShopSale = dateOfShopSale;
+        this.shopId = shopId;
+        this.weight = weight;
+        this.numberOfPages = numberOfPages;
+        this.cover = cover;
+        this.condition = condition;
+        this.moreInfo = moreInfo;
+        this.uploader = uploader;
+        this.category = category;
+        this.isbn = isbn;
+    }
+
+    //getters & setters
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(Integer yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Long getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(Long barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getStoragePlace() {
+        return storagePlace;
+    }
+
+    public void setStoragePlace(String storagePlace) {
+        this.storagePlace = storagePlace;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Timestamp getDateOfShopSale() {
+        return dateOfShopSale;
+    }
+
+    public void setDateOfShopSale(Timestamp dateOfShopSale) {
+        this.dateOfShopSale = dateOfShopSale;
+    }
+
+    public Integer getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Integer shopId) {
+        this.shopId = shopId;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getMoreInfo() {
+        return moreInfo;
+    }
+
+    public void setMoreInfo(String moreInfo) {
+        this.moreInfo = moreInfo;
+    }
+
+    public String getUploader() {
+        return uploader;
+    }
+
+    public void setUploader(String uploader) {
+        this.uploader = uploader;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "status='" + status + '\'' +
+                ", bookId='" + bookId + '\'' +
+                ", productId='" + productId + '\'' +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", yearOfPublication=" + yearOfPublication +
+                ", publisher='" + publisher + '\'' +
+                ", barcode=" + barcode +
+                ", storagePlace='" + storagePlace + '\'' +
+                ", price=" + price +
+                ", dateOfShopSale=" + dateOfShopSale +
+                ", shopId=" + shopId +
+                ", weight=" + weight +
+                ", numberOfPages=" + numberOfPages +
+                ", cover='" + cover + '\'' +
+                ", condition='" + condition + '\'' +
+                ", moreInfo='" + moreInfo + '\'' +
+                ", uploader='" + uploader + '\'' +
+                ", category='" + category + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
