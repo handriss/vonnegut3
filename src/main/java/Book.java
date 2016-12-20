ca4e1a0855fed15fc32d378f8b53a9fda88945a0
Mónika Lombos <lombos.monika@gmail.com>
2016-12-20 17:30:36 +0100

    Create Book basic model skeleton

---
+++ src/main/java/Book.java
@@ -0,0 +1,26 @@
+
+import java.sql.Timestamp;
+
+public class Book {
+
+    private String status;
+    private String book_id;
+    private String product_id;
+    private String author;
+    private String title;
+    private Integer yearOfPublication;
+    private String publisher;
+    private Long barcode;
+    private String storagePlace;
+    private Integer price;
+    private Timestamp dateOfShopSale;
+    private Integer shopId;
+    private Float weight;
+    private Integer numberOfPages;
+    private String cover;
+    private String condition;
+    private String moreinfo;
+    private String uploader;
+    private String category;
+    private String isbn;
+}
