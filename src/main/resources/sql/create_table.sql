26d655193e4fde97bf218bdec73c7bcbb37a3905
Mónika Lombos <lombos.monika@gmail.com>
2016-12-20 21:55:15 +0100

    Added extra fields & corrected the date type

--- src/main/resources/sql/create_table.sql
+++ src/main/resources/sql/create_table.sql
@@ -2,25 +2,26 @@ DROP TABLE IF EXISTS book;
 
 CREATE TABLE book
   (
-  id int(11) PRIMARY KEY AUTO_INCREMENT,
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
   status VARCHAR(40),
   book_id VARCHAR(30),
-  product_id VARCHAR(30),
-  author VARCHAR(100),
+  product_id DECIMAL,
+  author VARCHAR(200),
   title VARCHAR(200),
-  year_of_publication VARCHAR(100),
+  year_of_publication INT,
   publisher VARCHAR(200),
-  barcode VARCHAR(100),
+  barcode VARCHAR(14),
   storage_place VARCHAR(50),
-  price VARCHAR(100),
-  date_of_shop_sale VARCHAR(100),
+  price INT,
+  date_of_upload DATETIME,
+  date_of_shop_sale DATETIME DEFAULT NULL,
   shop_id VARCHAR(100),
-  weight VARCHAR(100),
-  number_of_pages VARCHAR(100),
+  weight FLOAT(4),
+  number_of_pages INT,
   cover VARCHAR(100),
-  book_condition VARCHAR(100),
-  more_info VARCHAR(100),
+  book_condition VARCHAR(20),
+  more_info VARCHAR(160),
   uploader VARCHAR(100),
   category VARCHAR(250),
-  isbn VARCHAR(50)
+  isbn VARCHAR(13)
 );
