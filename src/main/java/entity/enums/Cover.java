4632e0e7ff2d55f2f33c2f309d05279200bb946a
Mónika Lombos <lombos.monika@gmail.com>
2016-12-20 21:09:58 +0100

    Created enum for book's cover

---
+++ src/main/java/entity/enums/Cover.java
@@ -0,0 +1,19 @@
+package entity.enums;
+
+/**
+ * Created by lombocska on 2016.12.20..
+ */
+public enum Cover {
+    NINCS,
+    PAPÍR,
+    KEMÉNY_KÖTÉS,
+    VÁSZON,
+    FÉLVÁSZON,
+    LEPORELLÓ,
+    SPIRÁL,
+    TEXTIL,
+    PLÜSS,
+    FÉLMŰBŐR,
+    MŰBŐR,
+    BŐR
+}
