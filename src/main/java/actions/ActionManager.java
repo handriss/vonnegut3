2ebc6e7f00ce2d5c276a3f906ada61885538097c
handriss <andrashinkel@gmail.com>
2016-12-21 18:12:34 +0100

    actionmanager class skeleton created

---
+++ src/main/java/actions/ActionManager.java
@@ -0,0 +1,28 @@
+package actions;
+
+
+public class ActionManager {
+    private String username;
+    private String password;
+
+    public ActionManager(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public void login(){
+
+    }
+
+    public void logout(){
+
+    }
+
+    public void activate(){
+
+    }
+
+    public void deactivate(){
+
+    }
+}
