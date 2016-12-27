db253bc9eccdac85c336c432ec33a42b9f1eb9f1
Mónika Lombos <lombos.monika@gmail.com>
2016-12-27 11:36:04 +0100

    username and password added

--- src/main/java/ControlFromTerminal.java
+++ src/main/java/ControlFromTerminal.java
@@ -7,12 +7,15 @@ public class ControlFromTerminal {
 
     // to try the project in console
     public static void main(String[] args) {
-        System.out.println("username: ");
-        Scanner usernameScanner = new Scanner(System.in);
-        String username = usernameScanner.next();
-        System.out.println("password: ");
-        Scanner passwordScanner = new Scanner(System.in);
-        String password = passwordScanner.next();
+//        System.out.println("username: ");
+//        Scanner usernameScanner = new Scanner(System.in);
+//        String username = usernameScanner.next();
+//        System.out.println("password: ");
+//        Scanner passwordScanner = new Scanner(System.in);
+//        String password = passwordScanner.next();
+
+        String username = "menyhard";
+        String password = "nickhornby1";
 
         // login
         ActionManager actionManager = new ActionManager(username, password);
