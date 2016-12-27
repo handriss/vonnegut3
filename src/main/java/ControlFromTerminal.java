d60bd6a94db0c6402c5db8db91a29de33f75f9cc
handriss <andrashinkel@gmail.com>
2016-12-27 10:58:36 +0100

    login and activate tried out from console

--- src/main/java/ControlFromTerminal.java
+++ src/main/java/ControlFromTerminal.java
@@ -1,23 +1,22 @@
-import action_handling.ActionManager;
-import action_handling.actions.AuthenticationHandler;
+import actions.ActionManager;
+
+import java.util.Scanner;
 
 
 public class ControlFromTerminal {
 
     // to try the project in console
     public static void main(String[] args) {
-//        System.out.println("username: ");
-//        Scanner usernameScanner = new Scanner(System.in);
-//        String username = usernameScanner.next();
-//        System.out.println("password: ");
-//        Scanner passwordScanner = new Scanner(System.in);
-//        String password = passwordScanner.next();
+        System.out.println("username: ");
+        Scanner usernameScanner = new Scanner(System.in);
+        String username = usernameScanner.next();
+        System.out.println("password: ");
+        Scanner passwordScanner = new Scanner(System.in);
+        String password = passwordScanner.next();
 
-        String username = "menyhard";
-        String password = "nickhornby1";
+
         // login
-        AuthenticationHandler handler = new AuthenticationHandler(username, password);
-        ActionManager actionManager = new ActionManager(handler.login());
+        ActionManager actionManager = new ActionManager(username, password);
         actionManager.activate();
     }
 }
