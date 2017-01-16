853315d8f7e8c5b0eb765a5df4e15f0f47d07714
lombocska <lombos.monika@gmail.com>
2017-01-16 21:28:26 +0100

    Added index page

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,16 +2,28 @@ package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        System.out.println("valami");
+        return "login";
     }
 
+    @GetMapping("/")
+    public String renderIndex() {
+        System.out.println("valami");
+        return "index";
+    }
+
+
+    @PostMapping("/login")
+    public String getLoginData(@ModelAttribute Login login) {
+        return "result";
+
+    }
 }
---
+++ src/main/java/hello/Login.java
@@ -0,0 +1,15 @@
+package hello;
+
+/**
+ * Created by lombo on 2017. 01. 16..
+ */
+public class Login {
+    public String username;
+    public String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
